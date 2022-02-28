a2b0ac1c4577caebb9759a2323cc2564ae9e6f78
Kartikeya Mishra <kmishra@umd.edu>
2022-02-28 06:03:40 -0500

    removing obsolete code

--- canvas.py
+++ canvas.py
@@ -40,21 +40,5 @@ class Canvas:
             if isValid == False:
                 break
             
-        return isValid    
-    
-    # def drawAnimatedLineTest(self):
-    #     i = 0
-    #     while (1):
-    #         # press Escape or Space or Enter to exit
-    #         k = cv2.waitKey(1) & 0xFF
-    #         if k in Canvas._EXIT_PROGRAM_KEYS:
-    #             break
-            
-    #         coordStart = self.getCoordinatesInWorldFrame((i, i))
-    #         coordEnd = self.getCoordinatesInWorldFrame((i + 1, i + 1))
-    #         cv2.line(self._canvasArea , coordStart, coordEnd, (0, 255, 0), 1)
-    #         i += 1
-    #         cv2.imshow(CONSTANT.WINDOW_NAME, self._canvasArea)
-
-    #     cv2.destroyAllWindows()
+        return isValid
     
